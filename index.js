628b73415b3b95f8554e5007a7fb4b89c2c7dba7
shonan02 <66680460+shonan02@users.noreply.github.com>
2023-11-14 15:22:30 +0000

    Middleware

--- index.js
+++ index.js
@@ -4,12 +4,21 @@ const cors = require('cors');
 const Note = require('./models/note');
 const app = express();
 
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
 
+    if(error.name === 'CastError') {
+        return response.status(400).send({error: 'malformed id'});
+    } else if(error.name === 'Validation error') {
+        return response.status(400).json({ error: error.message});
+    }
+    next(error);
+}
 //Use static middleware to show static content
 app.use(express.static('dist'));
 app.use(express.json());
 app.use(cors());
-
+app.use(errorHandler);
 
 app.get('/api/notes', (req, res) => {
     Note.find({}).then(notes => {
@@ -17,8 +26,9 @@ app.get('/api/notes', (req, res) => {
     })
 })
 
-app.get('/api/notes/:id', (req, res) => {
-    Note.findById(req.params.id).then(note => {
+app.get('/api/notes/:id', (req, res, next) => {
+    Note.findById(req.params.id)
+    .then(note => {
         if(note) {
             res.json(note);
         } else {
@@ -28,12 +38,10 @@ app.get('/api/notes/:id', (req, res) => {
     .catch(err => next(err));
 })
 
-app.post('api/notes', (req, res, next) => {
-    const body = req.body;
-
+app.post('/api/notes', (req, res, next) => {
     const note = Note({
-        content: body.content,
-        important: body.important || false,
+        content: req.body.content,
+        important: req.body.important || false,
     })
 
     note.save().then(saved => {
@@ -51,25 +59,18 @@ app.delete('/api/notes/:id', (req, res, next) => {
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-    const body = request.body
+    const { content, important } = request.body;
   
-    const note = {
-      content: body.content,
-      important: body.important,
-    }
-  
-    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    Note.findByIdAndUpdate(
+        request.params.id, 
+        {content, important}, 
+        { new: true, runValidators: true, context: 'query' }
+    )
       .then(updatedNote => {
         response.json(updatedNote)
       })
       .catch(error => next(error))
   })
-
-  
-const generateId = () => {
-    const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0;
-    return maxId +1;
-}
 
 
 const PORT = process.env.PORT;
